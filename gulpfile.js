5ae5ee913af2ead44f2031bccc0f42c9cdde4090
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-02-15 15:52:27 -0800

    Updated paths to match new directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -15,49 +15,49 @@ var gulp        = require('gulp'),
 //////// Tasks used in development environment ////////
 // Scripts Task - tasks related to js
 gulp.task('scripts', function(){
-  gulp.src(['app/js/**/*.js', '!app/js/**/*min.js'])
+  gulp.src(['src/js/**/*.js', '!src/js/**/*min.js'])
   .pipe(plumber())
   .pipe(concat('script.min.js'))
-  .pipe(gulp.dest('app/js'))
+  .pipe(gulp.dest('src/js'))
   .pipe(uglify())
-  .pipe(gulp.dest('app/js'))
+  .pipe(gulp.dest('src/js'))
   .pipe(browserSync.stream());
 });
 
 // Sass Task - development css - nested/readable/mapped
 gulp.task('sassDev', function() {
-  gulp.src('app/sass/**/*.scss')
+  gulp.src('src/scss/**/*.scss')
   .pipe(plumber())
   .pipe(rename({suffix:'.dev'}))
   .pipe(sourcemaps.init())
     .pipe(sass({sourceComments: 'map', sourceMap: 'sass', outputStyle: 'nested'}))
     .pipe(autoprefixer('last 2 versions'))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('app/css/'))
+  .pipe(gulp.dest('src/css/'))
   .pipe(browserSync.stream());
 });
 
 // Sass Task - deployment css - compressed/minified/mapped
 gulp.task('sassDep', function(){
-  gulp.src('app/sass/**/*.scss')
+  gulp.src('src/scss/**/*.scss')
   .pipe(plumber())
   .pipe(sourcemaps.init())
     .pipe(sass({outputStyle: 'compressed'}))
     .pipe(autoprefixer('last 2 versions'))
   .pipe(sourcemaps.write())
-  .pipe(gulp.dest('app/css/'))
+  .pipe(gulp.dest('src/css/'))
   .pipe(browserSync.stream());
 });
 
 // Server Task - Asynchronous browser syncing of assets across multiple devices
 gulp.task('serve', function(){
   browserSync.init({
-    proxy   : "http://localhost/Bourbon/app"
+    proxy   : "http://localhost/Bourbon/src"
   });
 
-  gulp.watch('app/js/**/*.js', ['scripts']);
-  gulp.watch('app/sass/**/*.scss', ['sassDev']);
-  gulp.watch('app/sass/**/*.scss', ['sassDep']);
+  gulp.watch('src/js/**/*.js', ['scripts']);
+  gulp.watch('src/scss/**/*.scss', ['sassDev']);
+  gulp.watch('src/scss/**/*.scss', ['sassDep']);
   gulp.watch('**/*.html').on('change', browserSync.reload);
   gulp.watch('**/*.php').on('change', browserSync.reload);
 });
@@ -76,13 +76,13 @@ gulp.task('build:cleanfolder', function(){
 
 // create build directory for all files
 gulp.task('build:copy', ['build:cleanfolder'], function(){
-  return gulp.src(['./app/**/*/', '!./app/images/**/*' ])
+  return gulp.src(['./src/**/*/', '!./src/images/**/*' ])
   .pipe(gulp.dest('./build/'));
 });
 
 // minimize images in deployment directory
 gulp.task('build:imgMin', function(){
-    gulp.src('./app/images/**/*')
+    gulp.src('./src/images/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./build/images'));
 });
@@ -90,7 +90,7 @@ gulp.task('build:imgMin', function(){
 // remove unwanted build files and directories
 gulp.task('build:remove', ['build:copy'], function(done){
   del([  // list files and directories to delete
-    './build/sass/',
+    './build/scss/',
     './build/css/*dev*'
   ], done);
 });
