3a0b44a3e9a7346b20a26777ac19e492bb3dc78b
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-02-21 15:35:22 -0800

    Adjusted specificity of footer selector

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-			<footer class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
+			<footer id="main-footer" class="footer" role="contentinfo" itemscope itemtype="http://schema.org/WPFooter">
 
 				<div id="inner-footer" class="footer-container wrap cf">
 					<section class="sociocon">
