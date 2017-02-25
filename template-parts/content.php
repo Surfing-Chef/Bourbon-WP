7efb7188330fad2603defc09e83e02beb83be88a
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-02-24 22:48:23 -0800

    Changed blog post title size.  Enabled Feature Image if specified in post.

--- template-parts/content.php
+++ template-parts/content.php
@@ -15,7 +15,7 @@
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
@@ -27,14 +27,23 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
+		<?php the_content( sprintf(
 				/* translators: %s: Name of current post. */
 				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'bourbon-wp' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
+		?>
 
-			wp_link_pages( array(
+		<div class="post-thumbnail">
+	    <?php
+	      // check if the post or page has a Featured Image assigned to it.
+	      if ( has_post_thumbnail() ) {
+	          the_post_thumbnail('full');
+	      }
+	    ?>
+	  </div>
+		
+		<?php wp_link_pages( array(
 				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bourbon-wp' ),
 				'after'  => '</div>',
 			) );
