65d44d676208c3ada97b51ff9b8603d32a95bb93
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-03-01 23:12:51 -0800

    Added custom widget area for sidebar - not needed

--- functions.php
+++ functions.php
@@ -102,6 +102,16 @@ function bourbon_wp_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name'          => esc_html__( 'Contact Form', 'bourbon-wp' ),
+		'id'            => 'contact-form-landing',
+		'description'   => esc_html__( 'Use a text widget to place shortcode for Contact Form here.', 'bourbon-wp' ),
+		'before_widget' => '<section id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</section>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 add_action( 'widgets_init', 'bourbon_wp_widgets_init' );
 
