ec250f8d78f6b405b1ecfb89ece67792e4f47106
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-03-14 16:57:49 -0700

    Created template for homepage to display excerpts instead of whole posts.

--- template-parts/content-excerpt.php
+++ template-parts/content-excerpt.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Template part for displaying posts
+ * Template for displaying post excerpts on home page.
+ * Edit inc/template-tags.php to adjust layout markup
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
@@ -10,42 +11,45 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-		endif;
+	<div class="article-content">
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php bourbon_wp_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+		<section>
+			<?php
+			if ( is_single() ) :
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			else :
+				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+			endif;
+			?>
 
-	<div class="entry-content">
-		<?php the_excerpt(); ?>
+			<div class="entry-content">
+				<?php the_excerpt(); ?>
 
-		<div class="post-thumbnail">
-	    <?php
-	      // check if the post or page has a Featured Image assigned to it.
-	      if ( has_post_thumbnail() ) {
-	          the_post_thumbnail('full');
-	      }
-	    ?>
-	  </div>
+				<?php wp_link_pages( array(
+						'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bourbon-wp' ),
+						'after'  => '</div>',
+					) );
+				?>
+			</div><!-- .entry-content -->
+		</section>
 
-		<?php wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bourbon-wp' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<aside>
+			<header class="entry-header">
+				<?php
+				if ( 'post' === get_post_type() ) : ?>
+				<div class="entry-meta">
+					<?php bourbon_wp_posted_on(); ?>
+				</div><!-- .entry-meta -->
+				<?php
+				endif; ?>
+			</header><!-- .entry-header -->
 
-	<footer class="entry-footer">
-		<?php bourbon_wp_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+			<footer class="entry-footer">
+				<?php bourbon_wp_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
+
+		</aside>
+	</div>
+
+
 </article><!-- #post-## -->
