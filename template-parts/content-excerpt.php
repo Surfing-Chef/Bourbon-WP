70387573bc2ee712a54d7cba4c129a05bbf38a1a
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-03-14 11:55:23 -0700

    Adjust home page to display only excerpts

---
+++ template-parts/content-excerpt.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Bourbon-WP
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+		if ( is_single() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+		endif;
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php bourbon_wp_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_excerpt(); ?>
+
+		<div class="post-thumbnail">
+	    <?php
+	      // check if the post or page has a Featured Image assigned to it.
+	      if ( has_post_thumbnail() ) {
+	          the_post_thumbnail('full');
+	      }
+	    ?>
+	  </div>
+
+		<?php wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'bourbon-wp' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php bourbon_wp_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
