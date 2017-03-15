9d888ed47f1eb8d81b43efc15debd23c994dc1c9
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-03-15 10:08:48 -0700

    Added comment to markup

--- page.php
+++ page.php
@@ -18,6 +18,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<?php
+			// Start the loop.
 			while ( have_posts() ) : the_post();
 
 				get_template_part( 'template-parts/content', 'page' );
