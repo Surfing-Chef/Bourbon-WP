8a44aa38d3bb0f559d2ccb500a1117bd1e46e26b
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-03-27 17:06:05 -0700

    Implemented customized search form into Bourbon-WP

--- header.php
+++ header.php
@@ -61,13 +61,7 @@
 
 	      <div class="navigation-tools">
 	        <div class="search-bar">
-	          <form role="search">
-	            <input type="search" placeholder="Enter Search" />
-	            <button type="submit">
-	              <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/search-icon.png" alt="Search Icon">
-	            </button>
-	          </form>
-	          <!-- End search-bar FORM-->
+	          <?php bourbon_wp_search_form(); ?>
 	        </div>
 	        <!-- End search-bar -->
 
