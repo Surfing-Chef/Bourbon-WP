989fde85f9c9855a813ca710c637c8dbdf27d898
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-10 17:07:02 -0700

    Added markup for testing

--- tester-page.php
+++ tester-page.php
@@ -18,8 +18,49 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<!-- TEST MARKUP CAN GO AFTER THIS COMMENT -->
+			<section class="culinaria-widget-area">
+ 				<h2 class="widget-title">Recent Feeds</h2>
 
-			<!-- END TEST MARKUP AND CODE -->
+ 				<div class="textwidget">
+ 					<div class="vertical-tabs-container">
+
+ 					  <div class="vertical-tabs">
+ 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab is-active" rel="tab1">Saveur</a>
+ 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab2">Food52</a>
+ 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab3">Food &amp; Wine</a>
+ 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab4">Lucky Peach</a>
+ 							<a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab5">Cooks Illustrated</a>
+ 					  </div>
+ 					  <div class="vertical-tab-content-container">
+
+ 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading is-active" rel="tab1">Saveur</a>
+ 					    <div id="tab1" class="js-vertical-tab-content vertical-tab-content">
+ 								<div id="saveur"><!-- Apifier content --></div>
+ 					    </div>
+
+ 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab2">Food 52</a>
+ 					    <div id="tab2" class="js-vertical-tab-content vertical-tab-content">
+ 								<div id="food52"><!-- Apifier content --></div>
+ 							</div>
+
+ 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab3">Food &amp; Wine</a>
+ 					    <div id="tab3" class="js-vertical-tab-content vertical-tab-content">
+ 								<div id="foodandwine"><!-- Apifier content --></div>
+ 							</div>
+
+ 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab4">Lucky Peach</a>
+ 					    <div id="tab4" class="js-vertical-tab-content vertical-tab-content">
+ 								<div id="luckypeach"><!-- Apifier content --></div>
+ 							</div>
+ 							<a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab5">Cooks Illustrated</a>
+ 							<div id="tab5" class="js-vertical-tab-content vertical-tab-content">
+ 								<div id="cooksillustrated"><!-- Apifier content --></div>
+ 							</div>
+ 					  </div>
+ 					</div>
+				</div>
+			 </section>
+			 <!-- END TEST MARKUP AND CODE -->
 
 			<?php
 			// Start the loop.
