bdabcad1200f25371100a25614ba83d1c3f596c5
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-12 15:22:48 -0700

    Relocated posted-on content and cut out modified-on meta

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -12,29 +12,31 @@ if ( ! function_exists( 'bourbon_wp_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function bourbon_wp_posted_on() {
+	//$icon_posted = '<i class="fa fa-clock-o" aria-hidden="true"></i>';
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	}
+	// if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+	// 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+	// }
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
+		esc_html( get_the_date() )//,
+		//esc_attr( get_the_modified_date( 'c' ) ),
+		//esc_html( get_the_modified_date() )
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'bourbon-wp' ),
+		esc_html_x( '%s', 'post date', 'bourbon-wp' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'bourbon-wp' ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	);
+	// $byline = sprintf(
+	// 	esc_html_x( 'by %s', 'post author', 'bourbon-wp' ),
+	// 	'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+	// );
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	// echo '<span class="posted-on">' . $icon_posted . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
+	echo '<span class="posted-on meta">Posted on ' . $icon_posted . $posted_on . '</span><span class="byline"> ' . '</span>'; // WPCS: XSS OK.
 
 }
 
@@ -89,8 +91,6 @@ function bourbon_wp_posted_modified_on() {
 		);
 
 		echo '<span class="modified-on">' . $icon_modified . $modified_on . '</span>' ;
-
-		// echo '<span class="modified-on">' . $modified_on . '<span class="byline"> ' . $byline . '</span></span>'; //
 	}
 	echo '</span>';
 }
