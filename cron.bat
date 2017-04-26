78e85c76d0c66bc1e8de35592657ed151a726014
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-26 10:43:51 -0700

    Commented task

--- cron.bat
+++ cron.bat
@@ -1,6 +1,9 @@
 @echo off
 REM -----
-REM Created By Surfing-Chef www.businesslegions.com/blog
+REM Created By Surfing-Chef https://github.com/Surfing-Chef
+REM https://gist.github.com/Surfing-Chef/6eb9e3d4137362f7c543cb630e6f1d4e
 REM Cron job in WAMP
-REM Machine: ASUS x540s
+REM -----
+REM IMPORTANT!!  CHANGE THE PATH TO REFLECT THE MACHINE
+REM -----
 php -f C:\wamp64\www\Bourbon-WP\wp-content\themes\bourbon-wp\bot\feeds.php
