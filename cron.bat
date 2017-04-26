fe282d4fb31626ed05ad84fd0e13cba6991958de
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-26 10:35:51 -0700

    Created batch file to update feeds daily via wamp.  Used windows task scheduler, pointing to this batch file.

---
+++ cron.bat
@@ -0,0 +1,6 @@
+@echo off
+REM -----
+REM Created By Surfing-Chef www.businesslegions.com/blog
+REM Cron job in WAMP
+REM Machine: ASUS x540s
+php -f C:\wamp64\www\Bourbon-WP\wp-content\themes\bourbon-wp\bot\feeds.php
