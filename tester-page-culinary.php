276329209fbc45cdef6d3ed35837a6f530b35fb2
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-04-25 18:49:56 -0700

    Structured test page for gallery

--- tester-page-culinary.php
+++ tester-page-culinary.php
@@ -22,42 +22,24 @@ get_header(); ?>
  				<h2 class="widget-title">Recent Feeds</h2>
 
  				<div class="textwidget">
- 					<div class="vertical-tabs-container">
 
- 					  <div class="vertical-tabs">
- 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab is-active" rel="tab1">Saveur</a>
- 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab2">Food52</a>
- 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab3">Food &amp; Wine</a>
- 					    <a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab4">Lucky Peach</a>
- 							<a href="javascript:void(0)" class="js-vertical-tab vertical-tab" rel="tab5">Cooks Illustrated</a>
- 					  </div>
- 					  <div class="vertical-tab-content-container">
+					<!-- build the gallery here -->
+					<!-- use bourbon neat empties grid -->
+					<div class="grid gallery">
 
- 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading is-active" rel="tab1">Saveur</a>
- 					    <div id="tab1" class="js-vertical-tab-content vertical-tab-content">
- 								<div id="saveur"><!-- Apifier content --></div>
- 					    </div>
+						<?php
+							// retrieve array from file
+							get_feeds();
+							$feeds = file_get_contents(get_template_directory().'/bot/cache.txt');
+							$feeds = unserialize($feeds);
+							print_r($feeds);
 
- 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab2">Food 52</a>
- 					    <div id="tab2" class="js-vertical-tab-content vertical-tab-content">
- 								<div id="food52"><!-- Apifier content --></div>
- 							</div>
+							// loop through array to populate grid gallery
+						 ?>
 
- 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab3">Food &amp; Wine</a>
- 					    <div id="tab3" class="js-vertical-tab-content vertical-tab-content">
- 								<div id="foodandwine"><!-- Apifier content --></div>
- 							</div>
+					  <div class="grid-box-wrap"><div class="grid-box">image content</div></div>
+					</div>
 
- 					    <a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab4">Lucky Peach</a>
- 					    <div id="tab4" class="js-vertical-tab-content vertical-tab-content">
- 								<div id="luckypeach"><!-- Apifier content --></div>
- 							</div>
- 							<a href="" class="js-vertical-tab-accordion-heading vertical-tab-accordion-heading" rel="tab5">Cooks Illustrated</a>
- 							<div id="tab5" class="js-vertical-tab-content vertical-tab-content">
- 								<div id="cooksillustrated"><!-- Apifier content --></div>
- 							</div>
- 					  </div>
- 					</div>
 				</div>
 			 </section>
 			 <!-- END TEST MARKUP AND CODE -->
