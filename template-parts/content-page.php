3d5748fbfee4b30ea2875a153766b2a46c647176
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-05 23:33:47 -0700

    Deleted test code - the loop and category based post display

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -14,13 +14,6 @@
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
 
-	<!-- Add page-slug-dependent widget areas  -->
-	<!-- that are defined in functions.php -->
-	<?php if ( is_page( 'culinaria' )) : ?>
-		<?php bourbon_wp_postsbycategory('culinary') ?>
-	<?php endif;?>
-
-
 	<?php if ( is_page( 'culinaria' ) && (!function_exists('dynamic_sidebar') || !dynamic_sidebar("Culinaria Feeds")) ) : ?>
 	<?php endif;?>
 
