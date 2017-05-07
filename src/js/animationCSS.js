855ec6881dfb888034c1a615f9aebf95a0b0ce53
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-07 15:15:08 -0700

    Added functions to add hover and click animations

--- src/js/animationCSS.js
+++ src/js/animationCSS.js
@@ -1,10 +1,38 @@
-$(document).ready(function() {
-  var iconStartW = $(".icon").width();
-  var iconStartH = $(".icon").height();
-  var icon = $(".icon")
-  .hover( function(){
-    TweenLite.to(this, 0.1, {scaleX:2.05, scaleY:1.05});
-  }, function(){
-    TweenLite.to(this, 0.3, {scaleX:1, scaleY:1});
-  });
-});
+// CSS Animations
+
+  // reset on end
+  var animReset = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+
+  // hover animation
+  function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+      function() {
+        $(this).addClass('animated ' + animation).one(animReset, function(){
+          $(this).removeClass('animated ' + animation);
+        });
+      },
+      function(){
+        // mouseOut function
+      }
+    );
+  }
+
+  // click animation
+  function animationClick(element, animation){
+    element = $(element);
+    element.click(
+      function() {
+        $(this).addClass('animated ' + animation).one(animReset, function(){
+          $(this).removeClass('animated ' + animation);
+        });
+      }
+    );
+  }
+
+  // main Navigation
+  animationHover('.menu-image-title-after', 'pulse');
+
+  // footer icons
+  animationHover('.icon', 'flipInY');
+  animationClick('.icon', 'rubberBand');
