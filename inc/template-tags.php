875837db60137438c4a5db1882190ebb850e5e06
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-08 17:28:39 -0700

    Deleted commented out code

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -2,8 +2,6 @@
 /**
  * Custom template tags for this theme
  *
- * Eventually, some of the functionality here could be replaced by core features.
- *
  * @package Bourbon-WP
  */
 
@@ -12,17 +10,11 @@ if ( ! function_exists( 'bourbon_wp_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function bourbon_wp_posted_on() {
-	//$icon_posted = '<i class="fa fa-clock-o" aria-hidden="true"></i>';
 	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-	// if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-	// 	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
-	// }
 
 	$time_string = sprintf( $time_string,
 		esc_attr( get_the_date( 'c' ) ),
 		esc_html( get_the_date() )//,
-		//esc_attr( get_the_modified_date( 'c' ) ),
-		//esc_html( get_the_modified_date() )
 	);
 
 	$posted_on = sprintf(
@@ -30,12 +22,6 @@ function bourbon_wp_posted_on() {
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
-	// $byline = sprintf(
-	// 	esc_html_x( 'by %s', 'post author', 'bourbon-wp' ),
-	// 	'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
-	// );
-
-	// echo '<span class="posted-on">' . $icon_posted . $posted_on . '</span><span class="byline"> ' . $byline . '</span>';
 	echo '<span class="posted-on meta">Posted on ' . $icon_posted . $posted_on . '</span><span class="byline"> ' . '</span>'; // WPCS: XSS OK.
 
 }
