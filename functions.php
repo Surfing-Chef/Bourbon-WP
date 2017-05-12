675bd232a4f9c279568c565aed42d631077c7704
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-11 18:19:39 -0700

    Changed theme stylesheet to style.min.css from style.css

--- functions.php
+++ functions.php
@@ -123,7 +123,7 @@ add_action( 'widgets_init', 'bourbon_wp_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bourbon_wp_scripts() {
-	wp_enqueue_style( 'bourbon-wp-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bourbon-wp-style', get_template_directory_uri() . '/style.min.css' );
 
 	wp_enqueue_script( 'bourbon-wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
