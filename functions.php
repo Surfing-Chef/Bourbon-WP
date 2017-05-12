a90a04871ddc75fa46bec885bfeab7d874924a4e
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-11 23:18:43 -0700

    Toggled function.php bourbon-wp stylesheet to use

--- functions.php
+++ functions.php
@@ -123,6 +123,10 @@ add_action( 'widgets_init', 'bourbon_wp_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bourbon_wp_scripts() {
+
+	// developement
+	//wp_enqueue_style( 'bourbon-wp-style', get_template_directory_uri() . '/src/css/style.css' );
+	// deployed
 	wp_enqueue_style( 'bourbon-wp-style', get_template_directory_uri() . '/style.min.css' );
 
 	wp_enqueue_script( 'bourbon-wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
