b97636acf396ab7913f32384c1f5a47e50f536d5
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-05-21 23:15:05 -0700

    Spelling correction

--- functions.php
+++ functions.php
@@ -135,10 +135,10 @@ add_action( 'widgets_init', 'bourbon_wp_widgets_init' );
  */
 function bourbon_wp_scripts() {
 
-	// developement
+	// development
 	wp_enqueue_style( 'bourbon-wp-style', get_template_directory_uri() . '/src/css/style.css' );
 	// deployed
-	//wp_enqueue_style( 'bourbon-wp-style', get_template_directory_uri() . '/style.min.css' );
+	// wp_enqueue_style( 'bourbon-wp-style', get_template_directory_uri() . '/style.min.css' );
 
 	wp_enqueue_script( 'bourbon-wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
