457de1ae9951b195df782cf7dbae299ada51be94
Surfing-Chef <surfing-chef@surfing-chef.com>
2017-07-28 08:11:37 -0700

    Adjusted Culinaria widget properties

--- functions.php
+++ functions.php
@@ -127,17 +127,16 @@ register_sidebar( array(
 	'after_title'   => '</h2>',
 ) );
 
-// Widget Area - Weather
+// Widget Area - Culinaria
 register_sidebar( array(
 	'name'          => esc_html__( 'Weather Feeds', 'bourbon-wp' ),
 	'id'            => 'widget-area-3',
 	'description'   => esc_html__( 'Add widgets here.', 'bourbon-wp' ),
-	'before_widget' => '<section id="%1$s" class="widget weather-widget-area">',
+	'before_widget' => '<section id="%1$s" class="widget culinaria-widget-area">',
 	'after_widget'  => '</section>',
 	'before_title'  => '<h2 class="widget-title">',
 	'after_title'   => '</h2>',
 ) );
-}
 
 // Widget Area - Header Callout Content
 register_sidebar( array(
