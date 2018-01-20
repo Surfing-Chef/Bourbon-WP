160c9c36bb2f09ec6cee2eaa1f786fd1011cf8e6
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-01-20 15:34:42 -0800

    Copied expander code from refills.bourbon.io

---
+++ js/expander.js
@@ -0,0 +1,10 @@
+// EXPANDER
+$().ready(function()
+{
+    $(document).ready(function() {
+        $('.expander-trigger').click(function(){
+          $(this).toggleClass("expander-hidden");
+        });
+      });
+      
+}); // END EXPANDER
