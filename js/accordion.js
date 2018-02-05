003803c4ab6afe280440b52b353b21dd6073f856
Surfing-Chef <surfing-chef@surfing-chef.com>
2018-02-05 08:45:35 -0800

    Added custom jquery-ui accordion script to js directory

---
+++ js/accordion.js
@@ -0,0 +1,7 @@
+$().ready(function(){
+    
+    $(".cats").accordion({ active: false }, { collapsible: true }, { heightStyle: "content" });
+    $(".subs").accordion({ active: false }, { collapsible: true }, { heightStyle: "content" });
+    $(".preps").accordion({ active: false }, { collapsible: true }, { heightStyle: "content" });
+
+});
